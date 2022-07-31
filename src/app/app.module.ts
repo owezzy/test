d737627ddf63a21656d9925defa30aafbf12a060
owezzy <owenadira@gmail.com>
2022-07-31 22:18:27 +0300

    fix compilation problems from aws amplify library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { SortPipe } from './sort.pipe';
 import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
 import {Amplify} from "aws-amplify";
-// @ts-ignore
 import config from '../aws-exports.js';
 
 Amplify.configure(config);
