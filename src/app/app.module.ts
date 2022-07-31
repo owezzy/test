bbd4cde040ece3e2c116eea5be927ebdbc61c0ab
owezzy <owenadira@gmail.com>
2022-07-31 21:39:20 +0300

    implement AWS amplify auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortPipe } from './sort.pipe';
+import {AmplifyAuthenticatorModule} from "@aws-amplify/ui-angular";
+import {Amplify} from "aws-amplify";
+// @ts-ignore
+import config from '../aws-exports.js';
 
+Amplify.configure(config);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SortPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AmplifyAuthenticatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,9 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
+
+(window as any).Zone['__zone_symbol__ignoreConsoleErrorUncaughtError'] = true;
